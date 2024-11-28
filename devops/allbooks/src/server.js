ffa480102555b8a294db58b107060cc864abd351
Albert Richard <albertrml.de@gmail.com>
2024-11-27 23:35:28 -0300

    Altera o server

--- devops/allbooks/src/server.js
+++ devops/allbooks/src/server.js
@@ -19,5 +19,5 @@ server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
 console.log("Boas-vindas à API do allbooks")
-console.log("Acesse essa API em http://localhost:8000")
+console.log("API disponível em http://localhost:8000")
 })
